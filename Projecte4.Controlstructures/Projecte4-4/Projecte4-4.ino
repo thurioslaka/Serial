ffc86278f7a1884b88e013d1bcad16b537c6ca39
thurioslaka <ram-cas@hotmail.com>
2015-02-27 20:55:16 +0100

    Projecte4-4
    fet

---
+++ Projecte4.Controlstructures/Projecte4-4/Projecte4-4.ino
@@ -0,0 +1,39 @@
+float kgCO2m2 = 100;
+
+void setup()              // run once, when the sketch starts
+{
+  Serial.begin(9600);     // set up Serial library at 9600 bps
+
+  if ( kgCO2m2 < 3.5)
+  {
+    Serial.print("A!");
+  } 
+else if ( kgCO2m2 < 6.5)
+ {
+    Serial.print("B!");
+  } 
+else if ( kgCO2m2 < 11.1)
+ {
+    Serial.print("C!");
+  } 
+else if ( kgCO2m2 < 17.7)
+ {
+    Serial.print("D!");
+  }
+else if ( kgCO2m2 < 38.2)
+ {
+    Serial.print("E!");
+  }
+else if ( kgCO2m2 < 43.2)
+ {
+    Serial.print("F!");
+  }
+else
+ {
+    Serial.print("G!");
+  } 
+}
+void loop()   // we need this to be here even though its empty
+{
+}
+
